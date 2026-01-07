b75335d50c69ab673ff1b0b80804ab3e1e6f0265
RAHI BOT BBY <rahiahmed143@gmail.com>
2026-01-07 20:47:05 +0600

    Update developer credit and personal info in info.js

--- plugins/info.js
+++ plugins/info.js
@@ -7,7 +7,7 @@ module.exports = {
     permission: 0,
     prefix: 'both',
     categorie: 'Utilities',
-    credit: 'Developed by Mohammad Nayan',
+    credit: 'Developed by Mohammad 𝐑𝐚𝐡𝐢',
     usages: [`${global.config.PREFIX}info - Show admin and server information.`],
   },
   start: async ({ event, api, message }) => {
@@ -24,19 +24,20 @@ module.exports = {
 
       const infoMessage = `
 --------------------------------------------
-𝐍𝐚𝐦𝐞           : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧       : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐓𝐚𝐧𝐠𝐚𝐢𝐥, 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : 𝐌𝐨𝐲𝐦𝐨𝐧𝐬𝐢𝐧𝐡, 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
-𝐆𝐞𝐧𝐝𝐞𝐫       : 𝐌𝐚𝐥𝐞
-𝐀𝐠𝐞           : 𝟏𝟖+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤         : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mohammadnayan447@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩   : wa.me/+8801615298449
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦     : t.me/MOHAMMADNAYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤: https://www.facebook.com/www.xnxx.com169
+➥ 𝐇𝐞𝐲 𝐌𝐫/𝐦𝐢𝐬𝐬 
+╭────《  ᴍʏ ꜱᴇʟꜰ 》────⊷
+│ ╭──────✧❁✧──────◆
+│ │ 🌸 ɴᴀᴍᴇ :- 『 Mᴏʜᴀᴍᴍᴀᴅ Rꫝʜɪ  』
+│ │ 🏡 ғʀᴏᴍ :- 『  Sʏʟʜᴇᴛ 』
+│ │ 📘 ᴄʟᴀꜱs :- 『 Nᴏᴛ 』
+│ │ 💖 ʀʟs :- 『 Sɪɴɢʟᴇ Uʟᴛᴀ Pʀᴏ Mᴀx 』
+│ │ 🎯 ʜᴏʙʙʏ :- 『 Rɪᴅᴇ 』
+│ │ ☎️ ɴᴜᴍʙᴇʀ :- 『 01711209381 』
+│ │ ..ʏᴏᴜ ᴄᴀɴ ꜱᴇᴇ ᴍʏ ꜱᴛᴀᴛᴜꜱ..
+│ ╰──────✧❁✧──────◆
+╰══════════════════⊷
+--------------------------------------------
+--------------------------------------------
 
 --------------------------------------------
 \`\`\`
@@ -51,7 +52,7 @@ module.exports = {
 
       await api.sendMessage(
             event.threadId,
-            { image: { url: "https://i.postimg.cc/2y9bTqv6/retouch-2025071913433217.jpg" }, caption: infoMessage || '' },
+            { image: { url: "https://i.postimg.cc/J4cvqfDx/IMG-20251001-WA0001.jpg" }, caption: infoMessage || '' },
             { quoted: event.message }
           );;
     } catch (error) {
