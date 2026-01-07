32836ff0ae93b2a2cb16745b27efbb3eaac5944b
RAHI BOT BBY <rahiahmed143@gmail.com>
2026-01-07 20:52:04 +0600

    Update promotion message to include powered by note

--- grpUpCmd/promote.js
+++ grpUpCmd/promote.js
@@ -5,7 +5,8 @@ module.exports = {
     console.log(event);
     for (const member of promotedMembers) {
       await api.sendMessage(event.id, {
-        text: `🎉 Congratulations @${member.split('@')[0]}! You are now an admin!`,
+        text: `🎉 Congratulations @${member.split('@')[0]}! You are now an admin
+├─➤ Powered by Mᴏʜᴀᴍᴍᴀᴅ Rꫝʜɪ!`,
         mentions: [member]
       });
     }
